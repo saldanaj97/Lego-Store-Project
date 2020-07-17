4688f34d1aed9833cb3f3cd23e9d32df8e1ca940
Juan Saldana <saldanaj97@gmail.com>
2020-07-17 17:01:20 -0500

    Connected frontend CLI to backend database

---
+++ dbConnect.py
@@ -0,0 +1,15 @@
+import pyodbc
+cnx = pyodbc.connect(
+    server="localhost",
+    database="legoStore",
+    user='sa',
+    tds_version='7.4',
+    password="Juan1997",
+    port=1433,
+    driver='/usr/local/lib/libtdsodbc.so'
+)
+cursor = cnx.cursor()
+cursor.execute('SELECT * FROM legoStore.dbo.test')
+
+for row in cursor:
+    print(row)
